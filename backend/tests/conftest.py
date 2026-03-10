5db5da7770e1586800f700675f9f267349a4c9d4
らぐな <pasupoto17@gmail.com>
2026-03-10 17:00:10 +0900

    Initial commit

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -24,8 +24,18 @@ def event_loop():
 async def db_engine():
     DATABASE_URL = os.getenv("DATABASE_URL")
     engine = create_async_engine(DATABASE_URL, echo=True)
+
+    # テーブルを作る
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
     yield engine
-    await engine.dispose() 
+
+    # テーブルを削除
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+    await engine.dispose()
 
 
 @pytest.fixture(scope="function")
